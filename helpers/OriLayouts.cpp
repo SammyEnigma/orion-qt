c4adc59547ec7a69cac5b113c769dbc1deceaa12
Chunosov <N.Chunosov@yandex.ru>
2026-08-19 10:45:16 +0700

    add grid layout helper

--- helpers/OriLayouts.cpp
+++ helpers/OriLayouts.cpp
@@ -42,33 +42,128 @@ LayoutItem::LayoutItem(const char* label)
     _widget = label ? new QLabel(label) : nullptr;
 }
 
+class LayoutItemPlacer
+{
+public:
+    static void add(const LayoutItem& item, QBoxLayout* layout)
+    {
+        switch (item._mode)
+        {
+        case LayoutItemMode::Layout:
+            if (item._layout)
+                layout->addLayout(item._layout);
+            break;
+    
+        case LayoutItemMode::Widget:
+            if (item._widget)
+                layout->addWidget(item._widget);
+            break;
+    
+        case LayoutItemMode::Stretch:
+            layout->addStretch();
+            break;
+    
+        case LayoutItemMode::Space:
+            layout->addSpacing(item._space);
+            break;
+        }
+    }
+
+    static void add(const LayoutItem& item, QGridLayout* layout, int row, int col)
+    {
+        switch (item._mode)
+        {
+        case LayoutItemMode::Layout:
+            if (item._layout)
+                layout->addLayout(item._layout, row, col);
+            break;
+        case LayoutItemMode::Widget:
+            if (item._widget)
+                layout->addWidget(item._widget, row, col);
+            break;
+        case LayoutItemMode::Stretch:
+        case LayoutItemMode::Space:
+            break;
+        }
+    }
+};
+
 //------------------------------------------------------------------------------
-//                                    LayoutBox
+//                                   LayoutBase
 //------------------------------------------------------------------------------
 
-LayoutBox& LayoutBox::setDefMargins()
+void LayoutBase::_setDefMargins()
 {
     auto style = qApp->style();
-    boxLayout()->setContentsMargins(
+    _layout->setContentsMargins(
         style->pixelMetric(QStyle::PM_LayoutLeftMargin),
         style->pixelMetric(QStyle::PM_LayoutTopMargin),
         style->pixelMetric(QStyle::PM_LayoutRightMargin),
         style->pixelMetric(QStyle::PM_LayoutBottomMargin));
-    return *this;
 }
 
-LayoutBox& LayoutBox::setDefSpacing(qreal factor)
+void LayoutBase::_setDefSpacing(qreal factor)
 {
     bool vert = qobject_cast<QVBoxLayout*>(_layout);
-    boxLayout()->setSpacing(qRound(defSpacing(vert) * factor));
-    return *this;
+    _layout->setSpacing(qRound(defSpacing(vert) * factor));
 }
 
-QGroupBox* LayoutBox::makeGroupBox(const QString& title)
+QWidget* LayoutBase::makeWidget() const
+{
+    auto w = new QWidget;
+    w->setLayout(_layout);
+    return w;
+}
+
+QSharedPointer<QWidget> LayoutBase::makeWidgetAuto() const
+{
+    auto w = new QWidget;
+    w->setLayout(_layout);
+    return QSharedPointer<QWidget>(w);
+}
+
+QGroupBox* LayoutBase::makeGroupBox(const QString& title) const
 {
     auto gb = new QGroupBox(title);
     gb->setLayout(_layout);
     return gb;
+}
+
+//------------------------------------------------------------------------------
+//                                   LayoutBox
+//------------------------------------------------------------------------------
+
+void LayoutBox::add(const LayoutItem& item)
+{
+    LayoutItemPlacer::add(item, boxLayout());
+}
+
+void LayoutBox::add(const LayoutItems& items)
+{
+    auto layout = boxLayout();
+    for (const LayoutItem& item : items)
+        LayoutItemPlacer::add(item, layout);
+}
+
+//------------------------------------------------------------------------------
+//                                    Grid
+//------------------------------------------------------------------------------
+
+Grid::Grid(const std::initializer_list<LayoutItems>& items) : LayoutBase(new QGridLayout)
+{
+    auto layout = gridLayout();
+
+    int row = 0;
+    for (const auto& rowItems : items)
+    {
+        int col = 0;
+        for (const auto& item : rowItems)
+        {
+            LayoutItemPlacer::add(item, layout, row, col);
+            col++;
+        }
+        row++;
+    }
 }
 
 } // namespace Ori
--- helpers/OriLayouts.h
+++ helpers/OriLayouts.h
@@ -3,6 +3,7 @@
 
 #include <QWidget>
 #include <QBoxLayout>
+#include <QGridLayout>
 
 QT_BEGIN_NAMESPACE
 class QGroupBox;
@@ -21,30 +22,6 @@ public:
     LayoutItem(const QString& label);
     LayoutItem(const char* label);
 
-    void addTo(QBoxLayout* layout) const
-    {
-        switch (_mode)
-        {
-        case LayoutItemMode::Layout:
-            if (_layout)
-                layout->addLayout(_layout);
-            break;
-
-        case LayoutItemMode::Widget:
-            if (_widget)
-                layout->addWidget(_widget);
-            break;
-
-        case LayoutItemMode::Stretch:
-            layout->addStretch();
-            break;
-
-        case LayoutItemMode::Space:
-            layout->addSpacing(_space);
-            break;
-        }
-    }
-
 protected:
     LayoutItem() {}
 
@@ -55,6 +32,8 @@ protected:
         QWidget* _widget;
         int _space;
     };
+    
+    friend class LayoutItemPlacer;
 };
 
 
@@ -89,41 +68,48 @@ public:
 };
 
 
-typedef const std::initializer_list<LayoutItem> LayoutItems;
+typedef std::initializer_list<LayoutItem> LayoutItems;
 
 
-class LayoutBox : public LayoutItem
+class LayoutBase : public LayoutItem
 {
 public:
-    LayoutBox(QBoxLayout* layout, LayoutItems items)
-    {
-        _mode = LayoutItemMode::Layout;
-        _layout = layout;
-        add(items);
-    }
+    QWidget* makeWidget() const;
+    QSharedPointer<QWidget> makeWidgetAuto() const;
+    QGroupBox* makeGroupBox(const QString& title) const;
+    
+protected:
+    LayoutBase(QLayout* layout) : LayoutItem(layout) {}
+    
+    void _setDefMargins();
+    void _setDefSpacing(qreal factor);
+    void _setMargin(int value) { _layout->setContentsMargins(value, value, value, value); }
+    void _setMargins(int left, int top, int right, int bottom) { _layout->setContentsMargins(left, top, right, bottom); }
+    void _setSpacing(int value) { _layout->setSpacing(value); }
+    void _useFor(QWidget* parent) { parent->setLayout(_layout); }
+};
 
-    LayoutBox& setDefMargins();
-    LayoutBox& setMargin(int value) { _layout->setContentsMargins(value, value, value, value); return *this; }
-    LayoutBox& setMargins(int left, int top, int right, int bottom) { _layout->setContentsMargins(left, top, right, bottom); return *this; }
-    LayoutBox& setSpacing(int value) { boxLayout()->setSpacing(value); return *this; }
-    LayoutBox& setDefSpacing(qreal factor = 1);
+
+class LayoutBox : public LayoutBase
+{
+public:
+    LayoutBox& setDefMargins() { _setDefMargins(); return *this; }
+    LayoutBox& setDefSpacing(qreal factor = 1) { _setDefSpacing(factor); return *this; }
+    LayoutBox& setMargin(int value) { _setMargin(value); return *this; }
+    LayoutBox& setMargins(int left, int top, int right, int bottom) { _setMargins(left, top, right, bottom); return *this; }
+    LayoutBox& setSpacing(int value) { _setSpacing(value); return *this; }
+    LayoutBox& useFor(QWidget* parent) { _useFor(parent); return *this; }
     LayoutBox& setStretchFactor(QWidget* w, int s) { boxLayout()->setStretchFactor(w, s); return *this; }
-    LayoutBox& useFor(QWidget* parent) { parent->setLayout(_layout); return *this; }
-    QWidget* makeWidget() { auto w = new QWidget; w->setLayout(_layout); return w; }
-    QSharedPointer<QWidget> makeWidgetAuto() { auto w = new QWidget; w->setLayout(_layout); return QSharedPointer<QWidget>(w); }
-    QGroupBox* makeGroupBox(const QString& title);
 
     QBoxLayout* boxLayout() const { return qobject_cast<QBoxLayout*>(_layout); }
 
-    void add(LayoutItem item)
-    {
-        item.addTo(boxLayout());
-    }
+    void add(const LayoutItem& item);
+    void add(const LayoutItems& items);
 
-    void add(LayoutItems items)
+protected:
+    LayoutBox(QBoxLayout* layout, const LayoutItems& items) : LayoutBase(layout)
     {
-        for (const LayoutItem& item : items)
-            item.addTo(boxLayout());
+        add(items);
     }
 };
 
@@ -131,14 +117,30 @@ public:
 class LayoutH : public LayoutBox
 {
 public:
-    LayoutH(LayoutItems items) : LayoutBox(new QHBoxLayout, items) {}
+    LayoutH(const LayoutItems& items) : LayoutBox(new QHBoxLayout, items) {}
 };
 
 
 class LayoutV : public LayoutBox
 {
 public:
-    LayoutV(LayoutItems items) : LayoutBox(new QVBoxLayout, items) {}
+    LayoutV(const LayoutItems& items) : LayoutBox(new QVBoxLayout, items) {}
+};
+
+
+class Grid : public LayoutBase
+{
+public:
+    Grid(const std::initializer_list<LayoutItems>& items);
+
+    Grid& setDefMargins() { _setDefMargins(); return *this; }
+    Grid& setDefSpacing(qreal factor = 1) { _setDefSpacing(factor); return *this; }
+    Grid& setMargin(int value) { _setMargin(value); return *this; }
+    Grid& setMargins(int left, int top, int right, int bottom) { _setMargins(left, top, right, bottom); return *this; }
+    Grid& setSpacing(int value) { _setSpacing(value); return *this; }
+    Grid& useFor(QWidget* parent) { _useFor(parent); return *this; }
+
+    QGridLayout* gridLayout() const { return qobject_cast<QGridLayout*>(_layout); }
 };
 
 } // namespace Ori
